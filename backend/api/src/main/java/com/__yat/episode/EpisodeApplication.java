12f92a921b86b4cbdbc132fb312d13df367dabea
w0uldy0u <spacekim01@gmail.com>
2026-01-20 15:48:47 +0900

    chore: 백엔드 프로젝트 초기화

---
+++ backend/api/src/main/java/com/__yat/episode/EpisodeApplication.java
@@ -0,0 +1,13 @@
+package com.__yat.episode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpisodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpisodeApplication.class, args);
+	}
+
+}
---
+++ backend/api/src/test/java/com/__yat/episode/EpisodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__yat.episode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpisodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
