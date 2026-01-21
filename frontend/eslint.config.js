f7b5cd176e5d324b1accbc78836a9f5860346d2f
arty0928 <arty0928@konkuk.ac.kr>
2026-01-21 13:58:11 +0900

    fix: 기본 설정 앞으로 이동 및 extends 키워드 대신 설정 객체 나열로 변경 #82

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,20 +5,19 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.configs(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
         plugins: {
-            js,
             "simple-import-sort": simpleImportSort,
         },
-        extends: ["js/recommended"],
-        languageOptions: { globals: globals.browser },
+        languageOptions: { ...globals.browser },
         rules: {
             "react/react-in-jsx-scope": "off",
             "simple-import-sort/imports": "error",
             "simple-import-sort/exports": "error",
         },
     },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
 );
