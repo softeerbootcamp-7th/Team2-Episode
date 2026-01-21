ae380c666cf863f5133b3e14da0bf34e60ad7501
EunseoPark <88071251+arty0928@users.noreply.github.com>
2026-01-21 14:10:48 +0900

    [FE] eslint 미적용 문법 수정 및 최신 문법 적용 (#83)
    * fix: false 대신 off로 변경
    
    * fix: tseslint.config로 변경 후 배열 객체 대신 객체 나열 #82
    
    * fix: 기본 설정 앞으로 이동 및 extends 키워드 대신 설정 객체 나열로 변경 #82

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,25 +1,23 @@
 import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default defineConfig([
+export default tseslint.configs(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
         plugins: {
-            js,
             "simple-import-sort": simpleImportSort,
         },
-        extends: ["js/recommended"],
-        languageOptions: { globals: globals.browser },
+        languageOptions: { ...globals.browser },
         rules: {
-            "react/react-in-jsx-scope": false,
+            "react/react-in-jsx-scope": "off",
             "simple-import-sort/imports": "error",
             "simple-import-sort/exports": "error",
         },
     },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-]);
+);
