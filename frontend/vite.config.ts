2b20920d6f8d38a7aba0408290db3bec817822bc
arty0928 <arty0928@konkuk.ac.kr>
2026-01-22 00:16:45 +0900

    chore: svg를 컴포넌트화해서 동적 색상 적용 위한 svgr plugin 설치

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,12 @@
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), svgr()],
     resolve: {
         alias: [
             { find: "@shared", replacement: path.resolve(__dirname, "src/shared") },
