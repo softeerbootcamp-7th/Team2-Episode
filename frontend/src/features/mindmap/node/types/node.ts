9ca989623532607454793a82819241ac1055d222
arty0928 <arty0928@konkuk.ac.kr>
2026-01-22 18:46:30 +0900

    feat: Node 컴포넌트에 NODE_COLORS 상수 배열 추가

--- frontend/src/features/mindmap/node/types/node.ts
+++ frontend/src/features/mindmap/node/types/node.ts
@@ -1,4 +1,7 @@
+import { NODE_COLORS } from "@features/mindmap/node/utils/colors";
+
 export type NodeComponentProps = {
     colorIndex?: number;
+    color?: (typeof NODE_COLORS)[number];
     className?: string;
 };
