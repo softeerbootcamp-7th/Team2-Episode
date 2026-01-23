e3095138949207956e66e6203be9d81d8d86bad2
arty0928 <arty0928@konkuk.ac.kr>
2026-01-23 09:56:48 +0900

    feat: click 이벤트 리스너 생성

--- frontend/src/features/mindmap/node/menu_node/MenuNodeButton.tsx
+++ frontend/src/features/mindmap/node/menu_node/MenuNodeButton.tsx
@@ -4,8 +4,12 @@ import type { NodeComponentProps } from "@features/mindmap/node/types/node";
 import Icon from "@shared/components/icon/Icon";
 
 export default function MenuNodeButton({ color, className }: NodeComponentProps) {
+    const handleMenu = () => {
+        //TODO: MENU
+    };
     return (
         <div
+            onClick={handleMenu}
             className={cn(
                 "w-3 h-3 cursor-pointer rounded-bl-md rounded-tr-md justify-center items-center flex",
                 getColorClass({ color }),
