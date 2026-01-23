3370283aa152081295cd19426562be1b09e6fd8d
Pakxe <64801796+pakxe@users.noreply.github.com>
2026-01-23 15:45:18 +0900

    [FE] CallToActionButton 컴포넌트 구현 (#97)
    * fix: color 잘못 작성되어있는 것 수정
    
    * design: 자주 사용되는 컬러 세트를 상수로 사용
    
    * feat: CallToActionButton 컴포넌트 개발

---
+++ frontend/src/shared/styles/color_set.ts
@@ -0,0 +1,21 @@
+export const COLOR_SET = {
+    primary_accent: "bg-cobalt-700 text-white",
+    primary: "bg-primary text-white",
+
+    secondary: "bg-cobalt-200 text-white",
+
+    tertiary: "bg-primary-bg-02 text-primary",
+    tertiary_outlined: "bg-primary-bg-02 text-primary outline-1 outline-primary",
+
+    quaternary: "bg-white text-gray-800",
+    quaternary_accent_outlined: "bg-white text-gray-800 outline-1 outline-primary",
+
+    basic: "bg-gray-200 text-gray-800",
+    basic_accent: "bg-primary text-gray-800",
+
+    alert: "bg-red-100 text-red-200",
+    notification: "bg-green-100 text-green-200",
+
+    white: "bg-white text-gray-800",
+    black: "bg-black text-white",
+};
