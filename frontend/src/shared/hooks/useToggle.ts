eebf678fa1cf868d2b9c3e7c0a0b3c06fdd221d6
pakxe <pigkill40@naver.com>
2026-01-24 23:36:50 +0900

    feat: 사용법이 더 직관적인 useState방식으로 useToggle의 return value를 변경

--- frontend/src/shared/hooks/useToggle.ts
+++ frontend/src/shared/hooks/useToggle.ts
@@ -1,32 +1,19 @@
-import { useReducer } from "react";
-
-type Action = { type: "ON" } | { type: "OFF" } | { type: "TOGGLE" };
+import { useReducer, useState } from "react";
 
 type Props = {
     defaultValue?: boolean;
 };
 
 const useToggle = ({ defaultValue = false }: Props = {}) => {
-    const [state, dispatch] = useReducer(toggleReducer, defaultValue);
+    const [isToggled, setIsToggled] = useState(defaultValue);
 
-    const setOn = () => dispatch({ type: "ON" });
-    const setOff = () => dispatch({ type: "OFF" });
-    const toggle = () => dispatch({ type: "TOGGLE" });
+    const actionHandler = {
+        on: () => setIsToggled(true),
+        off: () => setIsToggled(false),
+        toggle: () => setIsToggled((prev) => !prev),
+    };
 
-    return [state, { setOn, setOff, toggle }] as const;
+    return [isToggled, actionHandler] as const;
 };
 
 export default useToggle;
-
-function toggleReducer(state: boolean, action: Action): boolean {
-    switch (action.type) {
-        case "ON":
-            return true;
-        case "OFF":
-            return false;
-        case "TOGGLE":
-            return !state;
-        default:
-            return state;
-    }
-}
