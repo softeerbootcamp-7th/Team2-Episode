f2b4c373131f0e72758d50ff48d18c716dad0486
arty0928 <arty0928@konkuk.ac.kr>
2026-01-26 00:25:38 +0900

    refactor: children 추가해서 텍스트 변동되게

--- frontend/src/shared/components/Input/InputAddButton.tsx
+++ frontend/src/shared/components/Input/InputAddButton.tsx
@@ -3,14 +3,14 @@ import { ComponentPropsWithoutRef } from "react";
 
 type Props = ComponentPropsWithoutRef<"button"> & {};
 
-export default function InputAddButton({ ...rest }: Props) {
+export default function InputAddButton({ children, ...rest }: Props) {
     return (
         <button
             {...rest}
             className="w-147.5 h-12 flex items-center justify-center gap-2 typo-body-14-medium cursor-pointer bg-base-white border-2 border-dashed border-gray-300 rounded-xl text-text-placeholder"
         >
             <Icon name="ic_plus" size={18} />
-            에피소드 추가
+            {children || "에피소드 추가"}
         </button>
     );
 }
