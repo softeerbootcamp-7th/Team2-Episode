dbcfb164e72514422eb99547761aa5fc85531c22
w0uldy0u <woojoo@spacekim.org>
2026-01-25 16:21:23 +0900

    chore: properties 스캔해서 자동 등록

--- backend/api/src/main/java/com/yat2/episode/EpisodeApplication.java
+++ backend/api/src/main/java/com/yat2/episode/EpisodeApplication.java
@@ -1,14 +1,13 @@
 package com.yat2.episode;
 
-import com.yat2.episode.auth.oauth.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.yat2.episode"})
-@EnableConfigurationProperties(KakaoProperties.class)
+@ConfigurationPropertiesScan("com.yat2.episode")
 public class EpisodeApplication {
 
 	public static void main(String[] args) {
