47ec0ec50d63436e26d43fcfa6951fde8dea2893
arty0928 <arty0928@konkuk.ac.kr>
2026-01-26 18:38:35 +0900

    fix: AddNode 내부 AddNodeArrow 보이는 것도 hover state에서 css hover selector로 변경

--- frontend/src/features/mindmap/node/add_node/AddNode.tsx
+++ frontend/src/features/mindmap/node/add_node/AddNode.tsx
@@ -1,29 +1,21 @@
 import { type NodeComponentProps } from "@features/mindmap/node/types/node";
-import { ComponentPropsWithoutRef, useState } from "react";
+import { ComponentPropsWithoutRef } from "react";
 import AddNodeDot from "@features/mindmap/node/add_node/AddNodeDot";
 import AddNodeArrow from "@features/mindmap/node/add_node/AddNodeArrow";
+import { cn } from "@utils/cn";
 
 type Props = ComponentPropsWithoutRef<"div"> &
     NodeComponentProps & {
         direction: "left" | "right";
     };
 
-export default function AddNode({ color, direction, ...rest }: Props) {
-    const [isHovered, setIsHovered] = useState(false);
-
+export default function AddNode({ color, direction, className, ...rest }: Props) {
     return (
-        <div
-            {...rest}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            className="relative w-13.5 h-13.5 flex items-center justify-center"
-        >
-            <div className={`transition-opacity duration-300 ${isHovered ? "opacity-0" : "opacity-100"}`}>
+        <div {...rest} className={cn("relative w-13.5 h-13.5 flex items-center justify-center", className)}>
+            <div className="transition-opacity duration-300">
                 <AddNodeDot color={color} />
             </div>
-            <div
-                className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${isHovered ? "opacity-100" : "opacity-0"}`}
-            >
+            <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 hover:opacity-100 opacity-0">
                 <AddNodeArrow color={color} direction={direction} />
             </div>
         </div>
