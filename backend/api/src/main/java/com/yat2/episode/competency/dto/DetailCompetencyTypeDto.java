d9b7101d12221f733aa96f88827e44a26a82f1f3
kimhji <twin4185@naver.com>
2026-01-26 15:10:48 +0900

    fix: getter 누락으로 인한 문제 수정

--- backend/api/src/main/java/com/yat2/episode/competency/dto/DetailCompetencyTypeDto.java
+++ backend/api/src/main/java/com/yat2/episode/competency/dto/DetailCompetencyTypeDto.java
@@ -2,7 +2,9 @@ package com.yat2.episode.competency.dto;
 
 import com.yat2.episode.competency.CompetencyType;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class DetailCompetencyTypeDto {
     private int id;
