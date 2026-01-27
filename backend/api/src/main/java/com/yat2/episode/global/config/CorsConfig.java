2e55fc0586d2fe7f25cbd1b6e73df7f1315f5bd2
kimhji <twin4185@naver.com>
2026-01-27 14:14:09 +0900

    feat: allow origins 분리

--- backend/api/src/main/java/com/yat2/episode/global/config/CorsConfig.java
+++ backend/api/src/main/java/com/yat2/episode/global/config/CorsConfig.java
@@ -1,24 +1,20 @@
 package com.yat2.episode.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:5173",
-                        "http://localhost:5174",
-                        "http://localhost:5175",
-                        "http://localhost:5176",
-                        "http://localhost:5177",
-                        "http://localhost:5178",
-                        "http://localhost:5179"
+                        allowedOrigins
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
