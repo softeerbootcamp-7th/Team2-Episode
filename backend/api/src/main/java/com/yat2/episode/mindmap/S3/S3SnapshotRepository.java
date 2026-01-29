ae2200cfcd43a43c696ddc1a52c1aad594ce886c
kimhji <twin4185@naver.com>
2026-01-29 23:52:44 +0900

    refactor: 불필요한 import 문 삭제

--- backend/api/src/main/java/com/yat2/episode/mindmap/S3/S3SnapshotRepository.java
+++ backend/api/src/main/java/com/yat2/episode/mindmap/S3/S3SnapshotRepository.java
@@ -1,9 +1,7 @@
 package com.yat2.episode.mindmap.S3;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
