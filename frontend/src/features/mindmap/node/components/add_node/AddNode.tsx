247bb90989dd463d2c0fc0cbbacf1efd19f25d8f
EunseoPark <88071251+arty0928@users.noreply.github.com>
2026-01-30 10:32:57 +0900

    [FE] node 컴포넌트에 highlight 상태 추가 및 리뷰 반영 (#205)
    Co-authored-by: Pakxe <64801796+pakxe@users.noreply.github.com>

---
+++ frontend/src/features/mindmap/node/components/add_node/AddNode.tsx
@@ -0,0 +1,37 @@
+import { ComponentPropsWithoutRef } from "react";
+import AddNodeDot from "@features/mindmap/node/components/add_node/AddNodeDot";
+import AddNodeArrow from "@features/mindmap/node/components/add_node/AddNodeArrow";
+import { cn } from "@utils/cn";
+import { NodeColor } from "@features/mindmap/node/constants/colors";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const addNodeVariants = cva(
+    "relative w-13.5 h-13.5 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300",
+    {
+        variants: {
+            direction: {
+                left: "order-first",
+                right: "order-last",
+            },
+        },
+    },
+);
+
+type Props = ComponentPropsWithoutRef<"div"> &
+    VariantProps<typeof addNodeVariants> & {
+        color: NodeColor;
+        direction: "left" | "right";
+    };
+
+export default function AddNode({ color, direction, className, ...rest }: Props) {
+    return (
+        <div {...rest} className={cn(addNodeVariants({ direction }), className)}>
+            <div className="transition-opacity duration-300">
+                <AddNodeDot color={color} />
+            </div>
+            <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 hover:opacity-100 opacity-0">
+                <AddNodeArrow color={color} direction={direction} />
+            </div>
+        </div>
+    );
+}
