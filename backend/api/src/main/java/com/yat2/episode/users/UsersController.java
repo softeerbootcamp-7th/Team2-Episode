2db023fb261387d2406bfc597e3686ce586f39a1
w0uldy0u <woojoo@spacekim.org>
2026-01-30 17:18:24 +0900

    chore: 불필요한 import 제거

--- backend/api/src/main/java/com/yat2/episode/users/UsersController.java
+++ backend/api/src/main/java/com/yat2/episode/users/UsersController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
