3560ce978f686ef258e9f3a7872b334538662540
arty0928 <arty0928@konkuk.ac.kr>
2026-02-01 04:13:46 +0900

    fix: AddNode hover 시 보이는 기능 누락 복구

--- frontend/src/features/mindmap/node/add_node/AddNode.tsx
+++ frontend/src/features/mindmap/node/add_node/AddNode.tsx
@@ -1,19 +1,19 @@
 import { ComponentPropsWithoutRef } from "react";
 
-import AddNodeArrow from "@/features/mindmap/node/add_node/AddNodeArrow";
-import AddNodeDot from "@/features/mindmap/node/add_node/AddNodeDot";
-import { type NodeComponentProps } from "@/features/mindmap/node/types/node";
 import { cn } from "@/utils/cn";
+import { NodeColor } from "../constants/colors";
+import AddNodeDot from "../components/add_node/AddNodeDot";
+import AddNodeArrow from "../components/add_node/AddNodeArrow";
 
-type Props = ComponentPropsWithoutRef<"div"> &
-    NodeComponentProps & {
+type Props = ComponentPropsWithoutRef<"div"> & {
+        color : NodeColor
         direction: "left" | "right";
     };
 
 export default function AddNode({ color, direction, className, ...rest }: Props) {
     return (
-        <div {...rest} className={cn("relative w-13.5 h-13.5 flex items-center justify-center", className)}>
-            <div className="transition-opacity duration-300">
+        <div {...rest} className={cn("group relative w-13.5 h-13.5 flex items-center justify-center", className)}>
+            <div className="transition-opacity duration-300 opacity-0 group-hover:opacity-100">
                 <AddNodeDot color={color} />
             </div>
             <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 hover:opacity-100 opacity-0">
