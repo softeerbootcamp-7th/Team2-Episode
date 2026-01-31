d68f7d2fd977a39ceb5cd3eb46b3fcc83202bc5c
EunseoPark <88071251+arty0928@users.noreply.github.com>
2026-02-01 05:21:34 +0900

    [FE] VariantProps의 nullable 문제를 StrictProps로 해결 (#226)

--- frontend/src/features/mindmap/node/types/node.ts
+++ frontend/src/features/mindmap/node/types/node.ts
@@ -1,1 +1,7 @@
+import { DefinedVariantProps } from "@/shared/types/safe_variant_props";
+import { nodeVariants } from "@/features/mindmap/node/components/node/Node";
+
 export type NodeMode = "default" | "highlight" | "selected";
+
+export type Size = DefinedVariantProps<typeof nodeVariants>['size'];
+
