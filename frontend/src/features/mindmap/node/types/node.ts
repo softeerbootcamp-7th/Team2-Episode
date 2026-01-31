26b6a46c079f030fd21b8f25d9abdf46c029f20d
arty0928 <arty0928@konkuk.ac.kr>
2026-02-01 04:41:00 +0900

    refactor: 사용하지 않는 StrictVariantProps 제거

--- frontend/src/features/mindmap/node/types/node.ts
+++ frontend/src/features/mindmap/node/types/node.ts
@@ -1,15 +1,3 @@
-import { VariantProps } from "class-variance-authority";
-
 export type NodeMode = "default" | "highlight" | "selected";
 
 export type Size = "sm" | "md" | "lg";
-
-/**
- * 1. T를 cva 함수 타입으로 제한
- * 2. VariantProps<T>가 반환하는 객체 타입을 그대로 가져오기
- * 3. Mapped Types([K in keyof ...])를 사용하여 모든 값에서 null과 undefined를 제거
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type StrictVariantProps<T extends (...args: any) => any> = {
-    [K in keyof VariantProps<T>]: Exclude<VariantProps<T>[K], null | undefined>;
-};
