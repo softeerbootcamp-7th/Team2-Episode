fe1f3f283ee1ef6c8bf0c13dc625e21d7aacd5fa
arty0928 <arty0928@konkuk.ac.kr>
2026-02-01 05:09:45 +0900

    feat: 선택적 속성인 ? 제거 및 필수 속성으로 만들기

--- frontend/src/shared/types/safe_variant_props.ts
+++ frontend/src/shared/types/safe_variant_props.ts
@@ -4,3 +4,8 @@ import { VariantProps } from "class-variance-authority";
 export type SafeVariantProps<T extends (...args: any) => any> = {
     [K in keyof VariantProps<T>]: Exclude<VariantProps<T>[K], null>;
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type DefinedVariantProps<T extends (...args: any) => any> = {
+    [K in keyof VariantProps<T>]-?: Exclude<VariantProps<T>[K], null | undefined>;
+};
