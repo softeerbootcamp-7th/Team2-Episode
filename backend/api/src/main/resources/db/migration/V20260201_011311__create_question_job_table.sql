b651a8fee8606683d386ea385434dd2cda904885
kimhji <twin4185@naver.com>
2026-02-01 18:04:29 +0900

    fix: sql error 수정

--- backend/api/src/main/resources/db/migration/V20260201_011311__create_question_job_table.sql
+++ backend/api/src/main/resources/db/migration/V20260201_011311__create_question_job_table.sql
@@ -12,19 +12,3 @@ CREATE TABLE `question_job_mapping` (
                                     FOREIGN KEY (`job_id`) REFERENCES `job` (`id`)
                                         ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `question_job_mapping` (
-                                        `id` INT NOT NULL AUTO_INCREMENT,
-                                        `question_id` INT NOT NULL,
-                                        `job_id` INT NOT NULL,
-                                        PRIMARY KEY (`id`),
-                                        UNIQUE KEY `uq_question_job` (`question_id`, `job_id`),
-                                        INDEX `idx_question_job_job` (`job_id`),
-
-                                        CONSTRAINT `fk_qj_question`
-                                            FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
-                                                ON DELETE CASCADE,
-                                        CONSTRAINT `fk_qj_job`
-                                            FOREIGN KEY (`job_id`) REFERENCES `job` (`id`)
-                                                ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
