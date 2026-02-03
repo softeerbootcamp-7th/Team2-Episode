d2b743738956bbc1bc0249b9b3329f0dd61da43d
kimhji <twin4185@naver.com>
2026-02-04 01:27:27 +0900

    refactor: 중복 데이터가 많은 dto 삭제 및 presentation 로직 삭제

--- backend/api/src/main/java/com/yat2/episode/diagnosis/DiagnosisResultService.java
+++ backend/api/src/main/java/com/yat2/episode/diagnosis/DiagnosisResultService.java
@@ -1,19 +1,18 @@
 package com.yat2.episode.diagnosis;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
 import com.yat2.episode.diagnosis.dto.DiagnosisArgsReqDto;
-import com.yat2.episode.diagnosis.dto.DiagnosisSimpleNoDateDto;
+import com.yat2.episode.diagnosis.dto.DiagnosisSimpleDto;
 import com.yat2.episode.global.exception.CustomException;
 import com.yat2.episode.global.exception.ErrorCode;
 import com.yat2.episode.question.Question;
 import com.yat2.episode.question.QuestionRepository;
 import com.yat2.episode.user.User;
 import com.yat2.episode.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,7 +24,7 @@ public class DiagnosisResultService {
     private final UserService userService;
 
     @Transactional
-    public DiagnosisSimpleNoDateDto createDiagnosis(Long userId, DiagnosisArgsReqDto reqDto) {
+    public DiagnosisSimpleDto createDiagnosis(Long userId, DiagnosisArgsReqDto reqDto) {
         User user = userService.getUserOrThrow(userId);
         validateUserJob(user);
 
@@ -41,7 +40,7 @@ public class DiagnosisResultService {
                 questions.stream().map(q -> DiagnosisWeakness.newDiagnosisWeakness(diagnosisResult, q)).toList();
         diagnosisWeaknessRepository.saveAll(weaknesses);
 
-        return DiagnosisSimpleNoDateDto.of(diagnosisResult, weaknesses.size());
+        return DiagnosisSimpleDto.of(diagnosisResult, weaknesses.size());
     }
 
     private void validateUserJob(User user) {
--- backend/api/src/main/java/com/yat2/episode/diagnosis/dto/DiagnosisSimpleDto.java
+++ backend/api/src/main/java/com/yat2/episode/diagnosis/dto/DiagnosisSimpleDto.java
@@ -1,15 +1,14 @@
 package com.yat2.episode.diagnosis.dto;
 
-import java.time.LocalDateTime;
-
 import com.yat2.episode.diagnosis.DiagnosisResult;
+
+import java.time.LocalDateTime;
 
 public record DiagnosisSimpleDto(Integer diagnosisId, String diagnosisName, LocalDateTime createdAt,
                                  int lackCountOfCompetency) {
-    private static final String DIAGNOSIS_TEMPLATE = " 직무 진단 결과";
 
     public static DiagnosisSimpleDto of(DiagnosisResult diagnosisResult, int lackCountOfCompetency) {
-        return new DiagnosisSimpleDto(diagnosisResult.getId(), diagnosisResult.getJob().getName() + DIAGNOSIS_TEMPLATE,
-                                      diagnosisResult.getCreatedAt(), lackCountOfCompetency);
+        return new DiagnosisSimpleDto(diagnosisResult.getId(), diagnosisResult.getJob().getName(),
+                diagnosisResult.getCreatedAt(), lackCountOfCompetency);
     }
 }
