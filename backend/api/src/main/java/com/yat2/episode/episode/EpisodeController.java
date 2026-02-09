b0486df1b6eb1c021e6a90645cf15dcf3b23d1ec
w0uldy0u <woojoo@spacekim.org>
2026-02-09 18:47:04 +0900

    feat: 에피소드 생성 controller 구현

---
+++ backend/api/src/main/java/com/yat2/episode/episode/EpisodeController.java
@@ -0,0 +1,36 @@
+package com.yat2.episode.episode;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+import com.yat2.episode.episode.dto.EpisodeDetailRes;
+import com.yat2.episode.episode.dto.EpisodeUpsertReq;
+import com.yat2.episode.global.swagger.AuthRequiredErrors;
+
+import static com.yat2.episode.global.constant.RequestAttrs.USER_ID;
+
+@RestController
+@AuthRequiredErrors
+@RequiredArgsConstructor
+@RequestMapping("/")
+class EpisodeController {
+
+    private final EpisodeService episodeService;
+
+    @PutMapping("/mindmap/{mindmapId}/episode/{nodeId}")
+    public EpisodeDetailRes createEpisode(
+            @PathVariable UUID mindmapId,
+            @PathVariable UUID nodeId,
+            @RequestAttribute(USER_ID) long userId,
+            @RequestBody EpisodeUpsertReq episodeUpsertReq
+    ) {
+        return episodeService.upsertEpisode(nodeId, userId, mindmapId, episodeUpsertReq);
+    }
+}
