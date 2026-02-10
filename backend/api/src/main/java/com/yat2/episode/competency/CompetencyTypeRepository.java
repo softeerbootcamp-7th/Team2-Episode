f588665e350766a7a080ac66ed6f4b8a3937bafe
Kim Woojoo <spacekim01@gmail.com>
2026-02-10 11:25:08 +0900

    refactor: episode 도메인의 쿼리 레포지토리 위치 옮기기

--- backend/api/src/main/java/com/yat2/episode/competency/CompetencyTypeRepository.java
+++ backend/api/src/main/java/com/yat2/episode/competency/CompetencyTypeRepository.java
@@ -1,26 +1,12 @@
 package com.yat2.episode.competency;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface CompetencyTypeRepository extends JpaRepository<CompetencyType, Integer> {
-    @Query(
-            """
-                    SELECT DISTINCT ctId
-                    FROM Episode e
-                    JOIN e.competencyTypeIds ctId
-                    WHERE e.mindmapId = :mindmapId
-                    """
-    )
-    List<Integer> findByMindmapId(
-            @Param("mindmapId") UUID mindmapId
-    );
 
-
+    long countByIdIn(List<Integer> ids);
 }
--- backend/api/src/main/java/com/yat2/episode/episode/EpisodeRepository.java
+++ backend/api/src/main/java/com/yat2/episode/episode/EpisodeRepository.java
@@ -1,10 +1,25 @@
 package com.yat2.episode.episode;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 interface EpisodeRepository extends JpaRepository<Episode, EpisodeId> {
     List<Episode> findByMindmapIdAndIdUserId(UUID mindmapId, long userId);
+
+    @Query(
+            """
+                    SELECT DISTINCT ctId
+                    FROM Episode e
+                    JOIN e.competencyTypeIds ctId
+                    WHERE e.mindmapId = :mindmapId
+                    """
+    )
+    List<Integer> findCompetencyTypesByMindmapId(
+            @Param("mindmapId") UUID mindmapId
+    );
+
 }
