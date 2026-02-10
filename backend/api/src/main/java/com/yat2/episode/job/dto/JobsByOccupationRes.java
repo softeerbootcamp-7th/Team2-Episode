f01548f706d34ee32bfbfd3083b5dd467da810c6
Kim Woojoo <spacekim01@gmail.com>
2026-02-10 20:56:23 +0900

    refactor: Job DTO 이름 변경

--- backend/api/src/main/java/com/yat2/episode/job/dto/JobsByOccupationRes.java
+++ backend/api/src/main/java/com/yat2/episode/job/dto/JobsByOccupationRes.java
@@ -2,8 +2,8 @@ package com.yat2.episode.job.dto;
 
 import java.util.List;
 
-public record JobsByOccupationDto(
+public record JobsByOccupationRes(
         Integer occupationId,
         String occupationName,
-        List<JobDto> jobs
+        List<JobSummary> jobs
 ) {}
