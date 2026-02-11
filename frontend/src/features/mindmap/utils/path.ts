8fad6f4079abc5018dada927d4d71366dd12a75c
arty0928 <arty0928@konkuk.ac.kr>
2026-02-11 14:50:52 +0900

    feat: Interaction 과정에서 엣지와 node만 그려주는 InteractionLayer 컴포넌트

---
+++ frontend/src/features/mindmap/utils/path.ts
@@ -0,0 +1,4 @@
+export const getBezierPath = (x1: number, y1: number, x2: number, y2: number) => {
+    const cx = (x1 + x2) / 2;
+    return `M ${x1} ${y1} C ${cx} ${y1}, ${cx} ${y2}, ${x2} ${y2}`;
+};
