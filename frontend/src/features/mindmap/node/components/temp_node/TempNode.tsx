27dc17e7d986988c5bc172a396015387a7f166c5
arty0928 <arty0928@konkuk.ac.kr>
2026-02-11 10:51:27 +0900

    feat: type에 따라 ghost | new 인 TempNode 컴포넌트

---
+++ frontend/src/features/mindmap/node/components/temp_node/TempNode.tsx
@@ -0,0 +1,27 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { ComponentPropsWithoutRef } from "react";
+
+import { cn } from "@/utils/cn";
+
+type TempNodeProps = ComponentPropsWithoutRef<"div"> & VariantProps<typeof tempNodeVariants>;
+
+const tempNodeVariants = cva("", {
+    variants: {
+        type: {
+            new: "flex w-40 min-w-40 px-4.5 py-5 justify-center items-center gap-2.5 typo-body-14-medium rounded-xl border-2 border-gray-500 bg-base-white shadow-[0_0_15px_0_rgba(43,46,67,0.15)]",
+            ghost: "w-25 h-10.5 rounded-xl bg-node-blue-op-15",
+        },
+    },
+});
+
+export default function TempNode({ type = "ghost", className, ...rest }: TempNodeProps) {
+    const isGhost = type == "ghost";
+
+    const content = isGhost ? null : "새로운 노드";
+
+    return (
+        <div {...rest} className={cn(tempNodeVariants({ type }), className)}>
+            {content}
+        </div>
+    );
+}
