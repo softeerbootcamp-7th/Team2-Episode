18c544f5e07a5a1e0ed1529b88469f0ed093f0ad
kimhji <twin4185@naver.com>
2026-02-12 23:52:17 +0900

    feat: 연결이 유지된 채로 네트워크 단 에러가 난 경우, 해당 연결 종료 처리

--- backend/api/src/main/java/com/yat2/episode/collaboration/RelayHandler.java
+++ backend/api/src/main/java/com/yat2/episode/collaboration/RelayHandler.java
@@ -1,12 +1,14 @@
 package com.yat2.episode.collaboration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class RelayHandler extends AbstractWebSocketHandler {
@@ -20,6 +22,16 @@ public class RelayHandler extends AbstractWebSocketHandler {
     @Override
     public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
         messageService.processMessage(session, message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        log.error("WebSocket 전송 에러 발생: 세션 ID = {}, 원인 = {}",
+                session.getId(), exception.getMessage());
+
+        if (session.isOpen()) {
+            session.close(CloseStatus.SERVER_ERROR);
+        }
     }
 
     @Override
