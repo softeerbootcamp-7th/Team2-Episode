9b2fefb57221a6bc60b071414d02b098ed71dcce
kimhji <twin4185@naver.com>
2026-02-13 18:06:12 +0900

    feat: nodejs 서버 기초 세팅 - fastify 설치 - typescript 설치 - dockerfile 구현

---
+++ backend/yjs-runner/app/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify, {FastifyInstance} from 'fastify';
+
+const app: FastifyInstance = Fastify({logger: true});
+
+app.get('/ping', async () => {
+    return {message: 'pong'};
+});
+
+const start = async () => {
+    try {
+        await app.listen({port: 3000, host: '0.0.0.0'});
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
