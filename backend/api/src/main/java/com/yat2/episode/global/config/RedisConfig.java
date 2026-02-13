64318ba216f72ba8ef424f0b1667fa0c733eeffe
w0uldy0u <woojoo@spacekim.org>
2026-02-13 23:42:01 +0900

    feat: redis 템플릿 생성

---
+++ backend/api/src/main/java/com/yat2/episode/global/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.yat2.episode.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, byte[]> redisBinaryTemplate(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setValueSerializer(RedisSerializer.byteArray());
+        template.setHashValueSerializer(RedisSerializer.byteArray());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
