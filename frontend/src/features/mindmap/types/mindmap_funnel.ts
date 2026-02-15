050090175954876843dfd53d4d9d5c9c7d0db308
pakxe <pigkill40@naver.com>
2026-02-15 16:39:45 +0900

    feat: 퍼널 타입

---
+++ frontend/src/features/mindmap/types/mindmap_funnel.ts
@@ -0,0 +1,13 @@
+import { ActivityCategory, MindmapType } from "@/features/mindmap/types/mindmap";
+import { SelectFrom } from "@/shared/types/select_from";
+
+export type CreateMindmapFunnel = {
+    TYPE: { mindmapType?: MindmapType };
+    TEAM_DETAIL: { mindmapType: SelectFrom<MindmapType, "PUBLIC">; projectName?: string; episodes: string[] };
+    CATEGORY: {
+        mindmapType: SelectFrom<MindmapType, "PRIVATE">;
+        categories?: ActivityCategory[];
+        projectName?: string;
+        episodes?: string[];
+    };
+};
