2152e0ec89fe7a4bd3dec51210ea93d955d5c477
arty0928 <arty0928@konkuk.ac.kr>
2026-02-17 02:32:06 +0900

    feat: controllerBar 구현

---
+++ frontend/src/features/mindmap/components/bar/ControllerSideBar.tsx
@@ -0,0 +1,44 @@
+import { ComponentPropsWithoutRef, ReactNode } from "react";
+
+import Button from "@/shared/components/button/Button";
+import Icon from "@/shared/components/icon/Icon";
+import { cn } from "@/utils/cn";
+
+type ColProps = ComponentPropsWithoutRef<"li"> & {
+    upSlot?: ReactNode;
+    bottomSlot?: ReactNode;
+};
+
+function Col({ upSlot, bottomSlot, className, ...props }: ColProps) {
+    return (
+        <li className={cn("p-0 flex flex-col justify-between items-center gap-6", className)} {...props}>
+            <div className="flex flex-col gap-2 items-center">{upSlot}</div>
+            {bottomSlot}
+        </li>
+    );
+}
+
+const iconColor = "var(--color-gray-700)";
+
+export default function ControllerSideBar() {
+    return (
+        <Col
+            upSlot={
+                <>
+                    <Button variant="sidebar" borderRadius="xl" size="xs">
+                        <Icon name="ic_tool_fit" color={iconColor} />
+                    </Button>
+
+                    <Button variant="sidebar" borderRadius="xl" size="xs">
+                        <Icon name="ic_target" color={iconColor} />
+                    </Button>
+                </>
+            }
+            bottomSlot={
+                <Button variant="sidebar" borderRadius="xl" size="xs">
+                    <Icon name="ic_plus" color={iconColor} />
+                </Button>
+            }
+        />
+    );
+}
