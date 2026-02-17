5138fddee0e262acbdafbaaa6899b7580e526232
Kim Woojoo <spacekim01@gmail.com>
2026-02-17 15:37:01 +0900

    [BE] Competency type 응답에 id 추가 (#409)

--- backend/api/src/main/java/com/yat2/episode/competency/dto/CompetencyTypeRes.java
+++ backend/api/src/main/java/com/yat2/episode/competency/dto/CompetencyTypeRes.java
@@ -3,10 +3,12 @@ package com.yat2.episode.competency.dto;
 import com.yat2.episode.competency.CompetencyType;
 
 public record CompetencyTypeRes(
+        int id,
         CompetencyType.Category category,
         String competencyType
 ) {
     public static CompetencyTypeRes of(CompetencyType competencyType) {
-        return new CompetencyTypeRes(competencyType.getCategory(), competencyType.getTypeName());
+        return new CompetencyTypeRes(competencyType.getId(), competencyType.getCategory(),
+                                     competencyType.getTypeName());
     }
 }
