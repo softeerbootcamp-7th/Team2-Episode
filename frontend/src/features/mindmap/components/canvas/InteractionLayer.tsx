cd127c5abcc12d5f84dfe3e2449b701869a73932
arty0928 <arty0928@konkuk.ac.kr>
2026-02-18 03:47:24 +0900

    refactpr: new TempNode 사이즈 상수화

--- frontend/src/features/mindmap/components/canvas/InteractionLayer.tsx
+++ frontend/src/features/mindmap/components/canvas/InteractionLayer.tsx
@@ -1,6 +1,6 @@
 import DropIndicator from "@/features/mindmap/components/canvas/DropIndicator";
 import MovingNodeFragment from "@/features/mindmap/components/canvas/MovingNodeFragment";
-import TempNode from "@/features/mindmap/node/components/temp_node/TempNode";
+import TempNode, { TEMP_NODE_SIZE } from "@/features/mindmap/node/components/temp_node/TempNode";
 import { InteractionSnapshot } from "@/features/mindmap/types/interaction";
 import { NodeElement, NodeId } from "@/features/mindmap/types/node";
 
@@ -11,8 +11,6 @@ type InteractionLayerProps = {
 export default function InteractionLayer({ nodeMap, status }: InteractionLayerProps) {
     const { mode, draggingNodeId, dragDelta, dragSubtreeIds, baseNode, mousePos } = status;
 
-    // if (mode !== "dragging"
-    // || !draggingNodeId || !dragSubtreeIds) return null;
     if (mode === "idle") return null;
 
     return (
@@ -30,7 +28,13 @@ export default function InteractionLayer({ nodeMap, status }: InteractionLayerPr
                     transform={`translate(${mousePos.x}, ${mousePos.y})`}
                     style={{ pointerEvents: "none" }} // 마우스 이벤트 방해 금지
                 >
-                    <foreignObject x={-90} y={-40} width={180} height={80} className="overflow-visible">
+                    <foreignObject
+                        x={-90}
+                        y={-40}
+                        width={TEMP_NODE_SIZE.new.width}
+                        height={TEMP_NODE_SIZE.new.height}
+                        className="overflow-visible"
+                    >
                         <TempNode type="new" />
                     </foreignObject>
                 </g>
