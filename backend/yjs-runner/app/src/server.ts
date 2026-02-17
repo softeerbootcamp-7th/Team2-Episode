efda24e63df4cba04877109fc39998a332eff25c
kimhji <126762281+kimhji@users.noreply.github.com>
2026-02-18 00:33:22 +0900

    [BE] yjs-runner 서버 레디스 처리 및 테스트 코드 추가 (#410)
    Co-authored-by: Kim Woojoo <spacekim01@gmail.com>

--- backend/yjs-runner/app/src/server.ts
+++ backend/yjs-runner/app/src/server.ts
@@ -34,7 +34,8 @@ const consumer = new RedisStreamJobConsumer(redis, {
     jobStreamKey: process.env.JOB_STREAM_KEY!,
     groupName: process.env.JOB_GROUP_NAME!,
     consumerName: process.env.JOB_CONSUMER_NAME!,
-    roomIdField: process.env.JOB_ROOM_FIELD,
+    roomIdField: process.env.JOB_ROOM_FIELD ? process.env.JOB_ROOM_FIELD : "r",
+    maxRetries: process.env.JOB_MAX_TRY ? process.env.JOB_MAX_TRY as unknown as number : 5,
 });
 
 const worker = new SnapshotWorker({
