b23af2b7bb7b467f5fc660d9f7b7220b4a494ada
pakxe <pigkill40@naver.com>
2026-02-18 16:26:02 +0900

    feat: 빌드 타입 env에 추가

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,11 +1,17 @@
 interface AppEnv {
     VITE_API_BASE_URL: string;
     VITE_WS_BASE_URL: string;
+    MODE: "development" | "production" | string;
+    DEV: boolean;
+    PROD: boolean;
 }
 
 const env = import.meta.env as unknown as AppEnv;
 
 export const ENV = {
-    API_BASE_URL: env.VITE_API_BASE_URL || `invalid`, // 에러나게 아무 뻥값 넣었습니다.
-    WS_BASE_URL: env.VITE_WS_BASE_URL || `invalid`,
+    API_BASE_URL: env.VITE_API_BASE_URL || "invalid",
+    WS_BASE_URL: env.VITE_WS_BASE_URL || "invalid",
+    MODE: env.MODE,
+    IS_DEV: env.DEV,
+    IS_PROD: env.PROD,
 } as const;
