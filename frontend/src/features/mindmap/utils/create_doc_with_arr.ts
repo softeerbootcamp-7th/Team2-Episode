80c6882f4129cb17d24029af2f39da4c7b7aa9c5
pakxe <pigkill40@naver.com>
2026-02-18 17:18:50 +0900

    feat: 마인드맵 생성 시 초기 ydoc을 만들어주는 함수

---
+++ frontend/src/features/mindmap/utils/create_doc_with_arr.ts
@@ -0,0 +1,34 @@
+import * as Y from "yjs";
+
+import { TreeModel } from "@/features/mindmap/engine/TreeModel";
+import { ROOT_NODE_ID, YjsAdapter } from "@/features/mindmap/engine/YjsAdaptor";
+
+/**
+ * 마인드맵을 생성할 때 유저가 선택한 카테고리 또는 에피소드 목록으로 기본 ydoc 뼈대를 만들어 서버에 제공해야합니다.
+ * 이 뼈대를 생성해주는 함수입니다.
+ */
+export const makeDocWithArr = ({ name, items, mindmapId }: { name: string; items: string[]; mindmapId: string }) => {
+    const doc = new Y.Doc();
+    const adaptor = new YjsAdapter({
+        doc,
+        roomId: mindmapId,
+        rootContents: name,
+    });
+    const tree = new TreeModel(adaptor);
+
+    items.forEach((item, i) => {
+        const newNodeId = tree.attachTo({
+            baseNodeId: ROOT_NODE_ID,
+            direction: "child",
+            addNodeDirection: i % 2 === 0 ? "right" : "left",
+        });
+
+        if (newNodeId) {
+            tree.update(newNodeId, {
+                data: { contents: item },
+            });
+        }
+    });
+
+    return doc;
+};
