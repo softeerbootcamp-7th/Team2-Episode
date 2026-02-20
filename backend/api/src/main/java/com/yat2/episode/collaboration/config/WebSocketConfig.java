41a9498d07beca610c9801e1a91c5a17795e4607
Kim Woojoo <spacekim01@gmail.com>
2026-02-20 16:37:30 +0900

    [BE] 톰캣 웹소켓 버퍼 사이즈 수정 (#473)

--- backend/api/src/main/java/com/yat2/episode/collaboration/config/WebSocketConfig.java
+++ backend/api/src/main/java/com/yat2/episode/collaboration/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.yat2.episode.collaboration.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.yat2.episode.collaboration.ws.HandshakeInterceptor;
 import com.yat2.episode.collaboration.ws.RelayHandler;
@@ -23,5 +25,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(relayHandler, webSocketProperties.pathPrefix() + "/{mindmapId}")
                 .addInterceptors(handshakeInterceptor)
                 .setAllowedOriginPatterns(webSocketProperties.allowedOriginPatterns().toArray(new String[0]));
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+
+        container.setMaxTextMessageBufferSize(webSocketProperties.maxMessageSize());
+        container.setMaxBinaryMessageBufferSize(webSocketProperties.maxMessageSize());
+
+        return container;
     }
 }
