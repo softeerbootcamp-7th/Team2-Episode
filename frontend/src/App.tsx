d0e0bf286f174114722bf15e26956179a17fc30c
pakxe <pigkill40@naver.com>
2026-02-21 16:54:58 +0900

    feat: error boundary 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, isRouteErrorResponse, Outlet, RouterProvider, useRouteError } from "react-router";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 
 import { authMiddleWare } from "@/features/auth/middleware/auth_middleware";
 import { AuthProvider } from "@/features/auth/providers/AuthProvider";
@@ -9,6 +9,7 @@ import CreateMindmapFunnelPage from "@/features/mindmap/pages/CreateMindmapPage"
 import MindmapDetailPage from "@/features/mindmap/pages/MindmapDetailPage";
 import MindmapListPage from "@/features/mindmap/pages/MindmapListPage";
 import LoginPage from "@/features/user/login/pages/LoginPage";
+import RootErrorBoundary from "@/shared/components/RootErrorBoundary/RootErrorBoundary";
 import { Toaster } from "@/shared/components/ui/sonner";
 import { PATHS } from "@/shared/utils/route";
 
@@ -73,23 +74,3 @@ function App() {
 }
 
 export default App;
-
-function RootErrorBoundary() {
-    const error = useRouteError();
-
-    return (
-        <div className="flex flex-col items-center justify-center h-screen p-4 text-center">
-            <h1 className="text-2xl font-bold mb-2">문제가 발생했습니다! 😢</h1>
-            <p className="text-muted-foreground mb-4">
-                {isRouteErrorResponse(error)
-                    ? `${error.status} ${error.statusText}`
-                    : error instanceof Error
-                      ? error.message
-                      : "알 수 없는 에러가 발생했습니다."}
-            </p>
-            <button onClick={() => (window.location.href = "/")} className="px-4 py-2 bg-primary text-white rounded-md">
-                홈으로 돌아가기
-            </button>
-        </div>
-    );
-}
