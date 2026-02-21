e4191cce43a241d3a03b13c102a2b7db610cece5
w0uldy0u <woojoo@spacekim.org>
2026-02-21 20:51:17 +0900

    fix: sync일때 lastEntryId 초기화 문제 해결

--- backend/yjs-runner/app/src/services/SnapshotService.ts
+++ backend/yjs-runner/app/src/services/SnapshotService.ts
@@ -26,18 +26,19 @@ export class SnapshotService {
             }
             const newSnapshot = this.deps.yjs.buildUpdatedSnapshot(baseSnapshot, updates.updateFrameList);
             await this.commit(job.roomId, updates.lastEntryId, newSnapshot);
+            await this.deps.updateRepo.trim(job.roomId, updates.lastEntryId);
         } else if (job.type === JobType.SYNC) {
             let doc = this.deps.yjs.getUpdatedYDocFromSnapshot(baseSnapshot, updates.updateFrameList);
             doc = await this.deps.syncClient.sync(doc, job.roomId);
 
             const newSnapshot = this.deps.yjs.getSnapshotFromDoc(doc);
-            await this.commit(job.roomId, updates.lastEntryId, newSnapshot);
+            await this.commit(job.roomId, Date.now().toString(), newSnapshot);
+            await this.deps.updateRepo.trim(job.roomId, updates.lastEntryId);
         }
     }
 
     private async commit(roomId: string, lastEntryId: string, snapshot: Uint8Array): Promise<void> {
         await this.deps.storage.upload(roomId, lastEntryId, snapshot);
         await this.deps.lastEntryIdRepo.set(roomId, lastEntryId);
-        await this.deps.updateRepo.trim(roomId, lastEntryId);
     }
 }
